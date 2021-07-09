b0160da96d54cd48e5f9072d1acc4d491af0c577
DESKTOP-ALCRIDC\ttd85 <ssw03270@khu.ac.kr>
2021-07-10 03:38:08 +0900

    [NEW] added KeybordControl module

---
+++ Sources/Gotcha/KeybordControl.py
@@ -0,0 +1,46 @@
+import pyautogui
+import pyperclip
+
+def KeybordInputString(key : str) -> None:
+    print("keybord input string", key)
+    pyperclip.copy(key)
+    pyautogui.hotkey('ctrl', 'v')
+
+def KeybordInputKey(key : str) -> None:
+    print("keybord input key", key)
+    pyautogui.hotkey(key)
+
+def KeybordInputKeyDown(key : str) -> None:
+    print("keybord input key down", key)
+    pyautogui.keyDown(key)
+
+def KeybordInputKeyUp(key : str) -> None:
+    print("keybord input key up", key)
+    pyautogui.keyUp(key)
+
+def KeybordInputKeyPress(key : str, count : int = 1) -> None:
+    print("keybord input key press", key, count, "time")
+    pyautogui.press(key, count)
+
+keyList = ['\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
+')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
+'8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
+'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
+'browserback', 'browserfavorites', 'browserforward', 'browserhome',
+'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
+'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
+'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
+'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
+'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
+'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
+'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
+'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
+'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
+'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
+'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
+'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
+'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab',
+'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen',
+'command', 'option', 'optionleft', 'optionright']
--- Sources/Gotcha/main.py
+++ Sources/Gotcha/main.py
@@ -1,9 +1,17 @@
 import MouseControl
+import KeybordControl
 
 if __name__ == "__main__":
+    print("HelloWorld")
     # print(MouseControl.MousePosition())
-    # MouseControl.MouseMove(100, 200)
+    # MouseControl.MouseMove(1006, 331)
     # MouseControl.MouseClickLeft()
     # MouseControl.MouseClickRight()
     # MouseControl.MouseScroll(100)
     # MouseControl.MouseDrag(100, 100, 2)
+
+    # KeybordControl.KeybordInputString("가나다라")
+    # KeybordControl.KeybordInputKeyDown('ctrl')
+    # KeybordControl.KeybordInputKeyPress('v', 3)
+    # KeybordControl.KeybordInputKeyUp('ctrl')
+    # KeybordControl.KeybordInputKeyPress('v', 3)
